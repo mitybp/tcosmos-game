2b591e04e6f2621de26941ef978bf54855bc3412
Okaye Hako <dimitribpusch21@gmail.com>
2023-03-21 17:11:47 -0300

    new

---
+++ main.js
@@ -0,0 +1,12 @@
+let state = "initial";
+let main = document.getElementById("main");
+
+
+if(state === "initial"){
+    main.innerHTML=```
+        <h1 class="center title">ScoutGame</h1>
+        <section>
+
+        </section>
+    ```
+}
